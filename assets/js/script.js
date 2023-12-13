e14eb01ee11fff72cc3714635a92f68cadfadc11
Robin Ung <Robin_ung@hotmail.com>
2023-12-13 17:06:19 +0000

    Add id/class pathways to js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Setting up all class/id 
+// Header
+let h1Color = document.getElementById('h1-color');
+
+// Menu
+let reset = document.getElementById('reset');
+let easy = document.getElementById('easy');
+let normal = document.getElementById('normal');
+let hard = document.getElementById('hard');
+let difficulty = document.getElementById('difficulty');
+
+// Game-color
+let gameColors = document.getElementById('game-colors');
+let colorBox = document.getElementById('color-box');

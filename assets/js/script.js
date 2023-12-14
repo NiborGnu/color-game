dc3bd79db183fa2caf48422cb87dc227d49ab62e
Robin Ung <Robin_ung@hotmail.com>
2023-12-14 16:00:17 +0000

    Change colorbox and difficulty from id > class

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,11 @@ let h1Color = document.getElementById('h1-color');
 
 // Menu
 let reset = document.getElementById('reset');
-let easy = document.getElementById('easy');
-let normal = document.getElementById('normal');
-let hard = document.getElementById('hard');
-let difficulty = document.getElementById('difficulty');
+let easy = document.getElementsByClassName('easy');
+let normal = document.getElementsByClassName('normal');
+let hard = document.getElementsByClassName('hard');
+let difficulty = document.getElementsByClassName('difficulty');
 
 // Game-color
 let gameColors = document.getElementsByClassName('game-colors');
-let colorBox = document.getElementById('color-box');
+let colorBox = document.getElementsByClassName('color-box');

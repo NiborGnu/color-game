c1135ad3aa788be0f467b9b978671c725b374894
Robin Ung <Robin_ung@hotmail.com>
2023-12-16 16:25:07 +0000

    Add start game button and function, Fix spelling css map

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,18 @@ let difficulty = document.getElementsByClassName('difficulty');
 // Game-color
 let gameColors = document.getElementsByClassName('game-colors');
 let colorBox = document.getElementsByClassName('color-box');
+
+// Start Game button
+let startGameButton = document.getElementById('start-game');
+
+// Hide Game
+document.querySelector('header').style.display = 'none';
+document.querySelector('main').style.display = 'none';
+
+//** Function Start GameButton */ 
+startGameButton.addEventListener('click', function () {
+    startGameButton.style.display = 'none';
+
+    document.querySelector('header').style.display = 'block';
+    document.querySelector('main').style.display = 'block';
+});
